7acdc93a10e129639054a765316e697bf84e6c81
shilu <zoushilu@gmail.com>
2020-12-24 11:26:08 -0800

    nav and about

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <nav></nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
